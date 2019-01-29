14581e043b0e29e91b26fc805b23c7ac6be3e8e2
localadmin <sivlesegrob9@gmail.com>
2019-01-29 14:34:08 +0100

    Minor Formatting changes to code in IR sensor

--- Sensors and Actuators/Proximity Sensors/SHARP_2Y0A02_EXAMPLE/SHARP_2Y0A02_EXAMPLE.ino
+++ Sensors and Actuators/Proximity Sensors/SHARP_2Y0A02_EXAMPLE/SHARP_2Y0A02_EXAMPLE.ino
@@ -1,15 +1,15 @@
-#define IR_PIN  A0                        // Setting up pin to receive voltage from IR
+#define IR_PIN  A0                       // Setting up pin to receive voltage from IR
 
-int value, prev_value = - 10000;          // int values (read from analog port, both the current and the previous)
-int deviation = 0;                        // setting the minimum deviation between the measurements (0 by default)
-                                          // up to 512 (although that is pretty useless)
+int value, prev_value = - 10000;         // int values (read from analog port, both the current and the previous)
+int deviation = 0;                       // setting the minimum deviation between the measurements (0 by default)
+                                         // up to 512 (although that is pretty useless)
 double voltage_value, distance_value;    // Converted to Voltage 
 
 void setup() {
   // Serial Connection setup
   Serial.begin(9600);
   Serial.println("Lets begin our pressure example!");
-  pinMode(IR_PIN, INPUT); // setting pinmode to read analog value 
+  pinMode(IR_PIN, INPUT);                // setting pinmode to read analog value 
 
   deviation = 10;  // since there's a bit of a drift in the values if you put the same object over a certain period
                    // we ignore a divergence of around 1 percent around the previous value. 
@@ -49,8 +49,8 @@ double d = -0.6928;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(100);                   // Here we can set the sampling rate, right now 10 Hz
-  value = analogRead(IR_PIN);  // reading our analog voltage, careful we only have 10 bit resolution so each
+  delay(100);                       // Here we can set the sampling rate, right now 10 Hz
+  value = analogRead(IR_PIN);       // reading our analog voltage, careful we only have 10 bit resolution so each
                                     // measurement step is only 5V ÷ 1024, so our result will be 0 - 1023
 
   // if value is within the range of [ previous - σ , previous + σ], ignore it (if value is relatively the same)
@@ -60,8 +60,6 @@ void loop() {
    
   voltage_value = double((value*5)) / 1023; // converting to voltage [ 0, 5] v.
   
-
-  
   distance_value = convert_to_distance(voltage_value); // getting actual distance value(cm) (careful using this, accuracy may not be ideal) 
                                                        // due to the functioning of the sensor, once you're closer than around 20 cm, it will
                                                        // start predicting higher distances again. Be careful with this, this is something you can
@@ -70,10 +68,7 @@ void loop() {
 
     if(distance_value  < 20  || distance_value > 150 ) // We will ignore values outside the range of measurement, this will happen around 2.7 -0.4 v
     return; 
-                                          
-
-
-    
+                                            
   Serial.print("Distance: ");
   Serial.print(value);
   Serial.print(" (0 - 1023) steps,  ");
