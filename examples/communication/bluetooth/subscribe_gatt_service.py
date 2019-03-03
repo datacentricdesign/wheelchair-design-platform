5b830859b6a60181529098be3bb97753574dbbc2
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2019-03-03 14:39:03 +0100

    clean GATT subscribe code

--- examples/communication/bluetooth/subscribe_gatt_service.py
+++ examples/communication/bluetooth/subscribe_gatt_service.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 # Import required library
-import pygatt
-import signal
-from dotenv import load_dotenv
-import os
+import pygatt  # To access BLE GATT support
+import signal  # To catch the Ctrl+C and end the program properly
+import os  # To access environment variables
+from dotenv import \
+    load_dotenv  # To load the environment variables from the .env file
 
+# DCD Hub
 from dcd.entities.thing import Thing
 from dcd.entities.property_type import PropertyType
 
@@ -16,76 +18,66 @@ THING_TOKEN = os.environ['THING_TOKEN']
 BLUETOOTH_DEVICE_MAC = os.environ['BLUETOOTH_DEVICE_MAC']
 
 # UUID of the GATT characteristic to subscribe
-GATT_CHARACTERISTIC = "02118833-4455-6677-8899-AABBCCDDEEFF"
+GATT_CHARACTERISTIC_ORIENTATION = "02118833-4455-6677-8899-AABBCCDDEEFF"
 
 # Many devices, e.g. Fitbit, use random addressing, this is required to connect.
 ADDRESS_TYPE = pygatt.BLEAddressType.random
 
-def handle_data(handle, value_bytes):
+
+def find_or_create(property_name, property_type):
+    """Search a property by name, create it if not found, then return it."""
+    if my_thing.find_property_by_name(property_name) is None:
+        my_thing.create_property(name=property_name,
+                                 property_type=property_type)
+    return my_thing.find_property_by_name(property_name)
+
+
+def handle_orientation_data(handle, value_bytes):
     """
     handle -- integer, characteristic read handle the data was received on
     value_bytes -- bytearray, the data returned in the notification
     """
-    print("Received data: %s" % str(value_bytes))
+    print("Received data: %s (handle %d)" % (str(value_bytes), handle))
     values = [float(x) for x in value_bytes.decode('utf-8').split(",")]
-    my_property.update_values(values)
+    find_or_create("Left Wheel Orientation",
+                   PropertyType.THREE_DIMENSIONS).update_values(values)
 
-# List characteristics of a device
+
 def discover_characteristic(device):
+    """List characteristics of a device"""
     for uuid in device.discover_characteristics().keys():
         try:
             print("Read UUID" + str(uuid) + "   " + str(device.char_read(uuid)))
         except:
             print("Something wrong with " + str(uuid))
 
-def read_characteristic(characteristic_id):
-    return my_device.char_read(characteristic_id)
 
-# Instantiate a thing with its credential
+def read_characteristic(device, characteristic_id):
+    """Read a characteristic"""
+    return device.char_read(characteristic_id)
+
+
+def keyboard_interrupt_handler(signal_num, frame):
+    """Make sure we close our program properly"""
+    print("Exiting...".format(signal_num))
+    left_wheel.unsubscribe(GATT_CHARACTERISTIC_ORIENTATION)
+    exit(0)
+
+
+# Instantiate a thing with its credential, then read its properties from the DCD Hub
 my_thing = Thing(thing_id=THING_ID, token=THING_TOKEN)
-
-# We can read the details of our thing,
-# i.e. retrieving its information from the hub
 my_thing.read()
-
-# If you just registered your Thing on the DCD Hub,
-# it has only an id, a name and a type.
-print(my_thing.to_json())
-
-# If we have no properties, let's create a random one
-if my_thing.find_property_by_name("My left wheel orientation") is None:
-    # By specifying a property type, the DCD Hub will
-    # automatically generate the property dimensions
-    # (in this case, 3 generic dimensions)
-    my_property = my_thing.create_property(
-        name="My left wheel orientation",
-        property_type=PropertyType.THREE_DIMENSIONS)
-
-    # Let's have a look at the property, it should
-    # contains the name, a unique id and the dimensions
-    print(my_property.to_json())
-
-# Retrieve the property
-my_property = my_thing.find_property_by_name("My left wheel orientation")
-
-# Show the property
-print(my_property.to_json())
 
 # Start a BLE adapter
 bleAdapter = pygatt.GATTToolBackend()
 bleAdapter.start()
 
-# User the BLE adapter to connect to our device
-my_device = bleAdapter.connect(BLUETOOTH_DEVICE_MAC, address_type=ADDRESS_TYPE)
+# Use the BLE adapter to connect to our device
+left_wheel = bleAdapter.connect(BLUETOOTH_DEVICE_MAC, address_type=ADDRESS_TYPE)
 
 # Subscribe to the GATT service
-my_device.subscribe(GATT_CHARACTERISTIC, callback=handle_data)
-
-# Make sure we close our program properly
-def keyboard_interrupt_handler(signal_num, frame):
-    print("Exiting...".format(signal_num))
-    my_device.unsubscribe(GATT_CHARACTERISTIC)
-    exit(0)
+left_wheel.subscribe(GATT_CHARACTERISTIC_ORIENTATION,
+                     callback=handle_orientation_data)
 
 # Register our Keyboard handler to exit
 signal.signal(signal.SIGINT, keyboard_interrupt_handler)
