824014b2f2204084d7d80be7d413c89e225e11c2
Pablo Ferreras <yartgomad@gmail.com>
2019-03-06 17:17:36 +0000

    added execption

--- wheelchair3/example_pressure_HyM.py
+++ wheelchair3/example_pressure_HyM.py
@@ -31,6 +31,7 @@ ser = serial.Serial(
     baudrate = 9600,
     timeout = 2)
 
+
 # Read the next line from the serial port
 # and update the property values
 def serial_to_property_values():
@@ -40,19 +41,23 @@ def serial_to_property_values():
     if len(line_bytes) > 0:
         # Convert the bytes into string
         line = line_bytes.decode('utf-8')
-        # Split the string using commas as separator, we get a list of strings
-        values = line.split(',')
-        # Use the first element of the list as property id
-        property_id = values.pop(0)
-        #print('the property id is-' + property_id)
-        # Get the property from the thing
-        prop = my_thing.properties[property_id]
-        # If we find the property, we update the values (rest of the list)
-        if prop is not None:
-            prop.update_values([float(x) for x in values])
-        # Otherwise, we show a warning
-        else:
-            print('Warning: unknown property ' + property_id)
+        print(line)
+        try:
+            # Split the string using commas as separator, we get a list of strings
+            values = line.split(',')
+            # Use the first element of the list as property id
+            property_id = values.pop(0)
+            #print('the property id is-' + property_id)
+            # Get the property from the thing
+            prop = my_thing.properties[property_id]
+            # If we find the property, we update the values (rest of the list)
+            if prop is not None:
+                prop.update_values([float(x) for x in values])
+            # Otherwise, we show a warning
+            else:
+                print('Warning: unknown property ' + property_id)
+        except:
+            print("cant parse " + line)
     # Finally, we call this method again
     serial_to_property_values()
 
