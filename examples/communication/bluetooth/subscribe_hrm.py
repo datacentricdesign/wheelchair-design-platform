00d7544d0fa00b4d4cc6d49391082d075bb89026
dhavalshah02 <dhaval02shah@gmail.com>
2019-03-07 12:38:06 +0100

    HRM example now works with grafana

--- examples/communication/bluetooth/subscribe_hrm.py
+++ examples/communication/bluetooth/subscribe_hrm.py
@@ -15,11 +15,13 @@ def hexStrToInt(hexstr):
     val = int(hexstr[3:5], 16) #
     if ((val & 0x8000) == 0x8000): # treat signed 16bits
         val = -((val ^ 0xffff) + 1)
+        val = float(val)
     return val
 
 #put your hrm mac address here
 hrmMacAddress = "e9:aa:88:b4:b6:bc"
 
+#access your thing in the hub from this program
 load_dotenv()
 THING_ID = os.environ['THING_ID']
 THING_TOKEN = os.environ['THING_TOKEN']
@@ -36,25 +38,29 @@ my_thing.read()
 print(my_thing.to_json())
 
 # If we have no properties, let's create a random one
-if my_thing.find_property_by_name("My heart rate measurement") is None:
+if my_thing.find_property_by_name("My heart rate measurement 1") is None:
     # By specifying a property type, the DCD Hub will
     # automatically generate the property dimensions
     # (in this case, 3 generic dimensions)
     my_property = my_thing.create_property(
-        name="My heart rate measurement",
-        property_type=PropertyType.THREE_DIMENSIONS)
+        name="My heart rate measurement 1",
+        property_type=PropertyType.ONE_DIMENSION)
 
     # Let's have a look at the property, it should
     # contains the name, a unique id and the dimensions
     print(my_property.to_json())
 
 # Retrieve the property
-my_property = my_thing.find_property_by_name("My heart rate measurement")
+my_property = my_thing.find_property_by_name("My heart rate measurement 1")
 
 # Show the property
 print(my_property.to_json())
 
-# Spawn a child process with gatttool to control your BLE device. 
+# Spawn a child process with gatttool to control your BLE device.
+#Your hrm uses random addressing like most BLE devices.
+#gatttool is the application within debian(your rpi operating system)
+#to communicate with BLE devices. Other single alhabets are flags that you do
+#do not need to know of
 child = pexpect.spawn("sudo gatttool -t random -b {0} -I".format(hrmMacAddress) )
 
 #Connect to hrm
@@ -90,8 +96,11 @@ while True:
         child.expect("Notification handle = 0x000e value: ", timeout=5)
         child.expect("\r\n", timeout=5)
         print(child.before)
-        intvalue = hexStrToInt(child.before)
+        intvalue = [hexStrToInt(child.before)]
+        #print statement to check the hrm reading
         print(intvalue)
+        #udate new readings to grafana
+        my_property.update_values(intvalue)
     except KeyboardInterrupt:
         print("Exiting...")
         # Unsubscribe from characteristic before exiting program
