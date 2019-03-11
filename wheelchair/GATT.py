27806b77c3d4cd29ae6531e9c20897988716252b
William Hagen <williamhagen@hotmail.com>
2019-03-11 12:37:09 +0100

    Coolenaam update

--- wheelchair/GATT.py
+++ wheelchair/GATT.py
@@ -39,7 +39,7 @@ def handle_orientation_data(handle, value_bytes):
     """
     print("Received data: %s (handle %d)" % (str(value_bytes), handle))
     values = [float(x) for x in value_bytes.decode('utf-8').split(",")]
-    find_or_create("Left Wheel Orientation",
+    find_or_create("Coolenaam",
                     PropertyType.THREE_DIMENSIONS).update_values(values)
 
 
