5d6bd95b9aafbe10e01434877cc5b4a5a5505187
Pablo Ferreras <artgomad@gmail.com>
2019-03-20 17:34:35 +0100

    commit

--- wheelchair3/Bluetooth Communication/write_gatt_PabloPrueba.py
+++ wheelchair3/Bluetooth Communication/write_gatt_PabloPrueba.py
@@ -131,10 +131,10 @@ def serial_to_property_values():
 
                 if prop_name == "Button":
                     print(prop_value[0]);
-                    if int(prop_value[0]) == 1:
+                    if prop_value[0] == 1.0:
                         my_device.char_write(GATT_CHARACTERISTIC_LED, bytearray([0x1, 0x00, 0x00]))
                         print("sending 1")
-                    if int(prop_value[0]) == 0:
+                    if prop_value[0] == 0.0:
                         print("sending 0")
                         my_device.char_write(GATT_CHARACTERISTIC_LED, bytearray([0x00, 0x00, 0x00]))
 
