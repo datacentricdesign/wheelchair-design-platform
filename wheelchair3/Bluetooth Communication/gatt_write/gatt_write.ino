2b63c48ed851bb2847c6a8df45150df17f02e065
Pablo Ferreras <artgomad@gmail.com>
2019-03-20 18:20:33 +0100

    Added functions of breading and stop breading

--- wheelchair3/Bluetooth Communication/gatt_write/gatt_write.ino
+++ wheelchair3/Bluetooth Communication/gatt_write/gatt_write.ino
@@ -50,9 +50,12 @@ int32_t charid_led;
 
 // Neo Pixel
 #define LED_PIN 6
-Adafruit_NeoPixel LED_controller = Adafruit_NeoPixel( 1, LED_PIN, NEO_RGB + NEO_KHZ800); 
+#define NUM_LEDS 1
+Adafruit_NeoPixel LED_controller = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_RGB + NEO_KHZ800); 
 uint8_t R = 0, G = 0, B = 0;
 
+//Vibrator motor
+#define VIB_PIN 10
 
 // A small helper
 void error(const __FlashStringHelper*err) {
@@ -78,20 +81,106 @@ void BleGattRX(int32_t chars_id, uint8_t data[], uint16_t len) {
 
 
   if (chars_id == charid_led) {
-    Serial.println("Received on led characteristic: ");
-    Serial.write(data, len);
-    Serial.println(len);
+    //Todos estos commandos Serial solo sirven para escribir en el terminal lo que le llega por bluetooth a la feather
+    Serial.println("Data received on led characteristic: ");
+    //Uso Serial.write para escribir el nuevo valor de la caracteristica en bytes (como viene por bluetooth)
+    //De hecho me viene como un bytearray que puedo descomponer en data[0], data[1] y data[2]
+    //Serial.write(buf, len)----->   buf: an array to send to the terminal as a series of bytes // len: the length of the buffer
+    //Serial.write(data, len);
+    //Serial.println(len);
+
+    //el ",DEC" sirve para definir como quiero que me escriba en el terminal el valor de data[x], en este caso como un numero decimal (DEC)
+    /*
+    for (int i=0, i<len, i++){
+      Serial.println(data[i], DEC);
+    }
+    */
+    
     Serial.println(data[0], DEC);
-    Serial.println(data[1], DEC);
-    Serial.println(data[2], DEC);
-    LED_controller.setPixelColor( 0, LED_controller.Color( data[0], data[1], data[2] ) );
-    LED_controller.show();
+    //Serial.println(data[1], DEC);
+    //Serial.println(data[2], DEC);
+    
+
+    int behaviour = (int)data[0];
+
+    switch (behaviour){
+      case 1://start audio
+        respiration();
+        break;
+      /*case 2:
+        decreaseVibrationLeft();
+        break;
+        */
+      default:
+        off();
+        break;
+    }
   }
+}
+int counter = 0;
+int vibrationValue = 0;
+int vibrationDirection = 1; 
+
+void startVibrationLeft(){
+  
+  vibrationValue = 200;
+  analogWrite(VIB_PIN, vibrationValue);
+  for (int i=0;i<NUM_LEDS;i++){
+     LED_controller.setPixelColor( i, LED_controller.Color( 255, 0, 0 ) );
+     LED_controller.show();
+  }
+}
+
+void respiration(){
+  Serial.print("vibrationValue = ");
+  Serial.println(vibrationValue);
+  
+  if (vibrationValue > 200 || vibrationValue < 0){//max vibration || min vibration
+      vibrationDirection = -vibrationDirection;
+    };
+
+  if (millis() - counter > 1){//delay between increasing or decreasing vibration   
+    vibrationValue = vibrationValue + 10*vibrationDirection;
+    
+    analogWrite(VIB_PIN, vibrationValue);
+    
+    for (int i=0;i<NUM_LEDS;i++){
+       LED_controller.setPixelColor( i, LED_controller.Color( vibrationValue, 0, 0 ) );
+       LED_controller.show();
+    }
+    counter = millis();
+  }
+}
+
+void off(){
+   
+   analogWrite(VIB_PIN, 0);
+   for (int i=0;i<NUM_LEDS;i++){
+           LED_controller.setPixelColor( i, LED_controller.Color( 0, 0, 0 ) );
+           LED_controller.show();
+        }
+}
+
+
+void decreaseVibrationLeft(){
+  
+  if (vibrationValue <= 0){
+    vibrationValue = 0;
+    //Send response to pi telling to change behaviour
+  }else if (millis() - counter > 200){
+    vibrationValue--;
+    analogWrite(VIB_PIN, vibrationValue);
+    counter = millis();
+  }
+
+
 }
 
 void setup(void) {
   
   LED_controller.begin(); // We're starting up the library
+
+  pinMode(VIB_PIN, OUTPUT);
   
   LED_controller.setPixelColor( 0, LED_controller.Color( 0, 150, 0 ) );
   LED_controller.show();
@@ -115,9 +204,11 @@ void setup(void) {
     error( F("Callback requires at least 0.7.0") );
   }
 
-  // Adding Service 0x1234 with characteristic 0x2345
+  // Adding custom Service 0x1234 with characteristic 0x2345
   ble.sendCommandCheckOK( F("AT+GATTADDSERVICE=uuid=0x1234") );
   ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=0x2345,PROPERTIES=0x08,MIN_LEN=1,MAX_LEN=6,DATATYPE=string,DESCRIPTION=string,VALUE=abc"), &charid_led);
+  //PROPERTIES=0x08 is the Bit-field of the property and indicates how to interact with this property, 
+  //the 0x08 value defines the property as write, permiting writes of the characteristic value with response
 
   ble.reset();
 
