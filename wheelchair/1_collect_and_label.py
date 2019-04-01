62046c457d7836c90cbf5852f79e21e52bc37f8b
Rosan Foppen <rosanfoppen@gmail.com>
2019-04-01 15:41:15 +0200

    fix dim number

--- wheelchair/1_collect_and_label.py
+++ wheelchair/1_collect_and_label.py
@@ -17,8 +17,7 @@ THING_ID = os.environ['THING_ID']
 THING_TOKEN = os.environ['THING_TOKEN']
 
 # Sitting classes
-CLASSES = ["Not Sitting", "Jelle", "Rosan",
-           "William"]
+CLASSES = ["Not Sitting", "Jelle", "Rosan", "William"]
 
 LABEL_PROP_NAME = "Persona"
 DATA_PROP_NAME = "fsr"
@@ -105,7 +104,7 @@ prop_label = my_thing.find_or_create_property(LABEL_PROP_NAME, PropertyType.CLAS
 if prop_label.classes is None or len(prop_label.classes) == 0:
     prop_label.create_classes(CLASSES)
 
-prop_data = my_thing.find_or_create_property(DATA_PROP_NAME, PropertyType.TWELVE_DIMENSIONS)
+prop_data = my_thing.find_or_create_property(DATA_PROP_NAME, PropertyType.SIX_DIMENSIONS)
 
 
 # Start collecting data for the first class
