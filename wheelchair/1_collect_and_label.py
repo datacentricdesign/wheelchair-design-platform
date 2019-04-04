f96ab1096a54f11dea103cd9683c21a494aae3b7
Rosan Foppen <rosanfoppen@gmail.com>
2019-04-04 14:32:49 +0200

    dataset

--- wheelchair/1_collect_and_label.py
+++ wheelchair/1_collect_and_label.py
@@ -17,9 +17,9 @@ THING_ID = os.environ['THING_ID']
 THING_TOKEN = os.environ['THING_TOKEN']
 
 # Sitting classes
-CLASSES = ["Not Sitting", "Jelle", "Rosan", "William"]
+CLASSES = ["Unknown", "Jelle", "Rosan", "William"]
 
-LABEL_PROP_NAME = "Persona"
+LABEL_PROP_NAME = "Data.set"
 DATA_PROP_NAME = "fsr"
 
 # How many samples do we want for each class
