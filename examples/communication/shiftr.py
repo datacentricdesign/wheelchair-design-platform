fbd0a7296bc987ffbc062d3be73fec2f2d048e91
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2023-02-20 23:28:20 +0100

    merge shiftr and pulse

--- examples/communication/shiftr.py
+++ examples/communication/shiftr.py
@@ -3,6 +3,35 @@ import threading
 import random
 import time
 
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import board
+import busio
+
+# import adafruit_ads1x15.ads1015 as ADS
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+# Create the I2C bus
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the ADS object
+# ads = ADS.ADS1015(i2c)
+ads = ADS.ADS1115(i2c)
+
+# Create a single-ended channel on Pin 0
+#   Max counts for ADS1015 = 2047
+#                  ADS1115 = 32767
+chan = AnalogIn(ads, ADS.P0)
+
+rate = [0]*10
+amp = 100
+GAIN = 2/3  
+curState = 0
+stateChanged = 0
+
+
 from dotenv import dotenv_values
 config = dotenv_values(".env")
 
@@ -15,20 +44,93 @@ def on_connect(client, userdata, flags, rc):
     # client.subscribe("test")
     # client.publish("hello", payload="test wheelchair", qos=0, retain=False)
 
-    thread_hr = threading.Thread(target=collect_hr_data)
+    thread_hr = threading.Thread(target=read_pulse)
     thread_hr.start()
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print("Message on topic" + msg.topic+": "+str(msg.payload))
 
-def collect_hr_data():
-    while True:
-        on_hr_data(random.randint(0,100))
-        time.sleep(2)
-
-def on_hr_data(data):
+def on_pulse_data(data):
     client.publish("hr", payload=data, qos=0, retain=False)
+
+def read_pulse():
+    firstBeat = True
+    secondBeat = False
+    sampleCounter = 0
+    lastBeatTime = 0
+    lastTime = int(time.time()*1000)
+    th = 525
+    P = 512
+    T = 512
+    IBI = 600
+    Pulse = False
+
+    while True:
+        ads.gain = GAIN
+        Signal = chan.value
+        # print("{:5} {:5.3f}".format(chan.value, chan.voltage), end="")
+
+        # Signal = adc.read_adc(0, gain=GAIN)   
+        curTime = int(time.time()*1000)
+        # send_to_prcessing("S",Signal)
+        sampleCounter += curTime - lastTime
+        lastTime = curTime
+        N = sampleCounter - lastBeatTime
+
+        if Signal > th and  Signal > P:          
+            P = Signal
+     
+        if Signal < th and N > (IBI/5.0)*3.0 :  
+            if Signal < T :                      
+              T = Signal                                                 
+        
+        if N > 250 :                              
+            if  (Signal > th) and  (Pulse == False) and  (N > (IBI/5.0)*3.0)  :       
+              Pulse = 1;                       
+              IBI = sampleCounter - lastBeatTime
+              lastBeatTime = sampleCounter       
+
+              if secondBeat :                     
+                secondBeat = 0;               
+                for i in range(0,10):             
+                  rate[i] = IBI                      
+
+              if firstBeat :                        
+                firstBeat = 0                  
+                secondBeat = 1                  
+                continue                              
+
+              runningTotal = 0;               
+              for i in range(0,9):            
+                rate[i] = rate[i+1]       
+                runningTotal += rate[i]      
+
+              rate[9] = IBI;                  
+              runningTotal += rate[9]        
+              runningTotal /= 10;             
+              BPM = 60000/runningTotal       
+              print("BPM:" + str(BPM))
+              on_pulse_data(BPM)
+
+        if Signal < th and Pulse == 1 :                    
+            amp = P - T                   
+            th = amp/2 + T
+            T = th
+            P = th
+            Pulse = 0 
+            
+
+        if N > 2500 :
+            th = 512
+            T = th                  
+            P = th                                              
+            lastBeatTime = sampleCounter
+            firstBeat = 0                     
+            secondBeat = 0                   
+            print("no beats found")
+
+        time.sleep(0.005)
 
 client = mqtt.Client(config["CLIENT_ID"])
 client.on_connect = on_connect
